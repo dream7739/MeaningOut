89e8fc84845f794bf37e327bdc28ad5a4568e7ed
홍정민 <dream7739@naver.com>
2024-06-13 20:43:50 +0900

    런치스크린 UI 구성

---
+++ MeaningOut/Enum/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  MeaningOut
+//
+//  Created by 홍정민 on 6/13/24.
+//
+
+import UIKit
+
+enum Constant {
+    enum ColorType {
+        static let theme = UIColor(red: 239/255, green: 137/255, blue: 71/255, alpha: 1)
+        static let primary = UIColor(red: 76/255, green: 76/255, blue: 76/255, alpha: 1)
+        static let secondary = UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1)
+        static let tertiary = UIColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1)
+        static let black = UIColor.black
+        static let white = UIColor.white
+    }
+}
