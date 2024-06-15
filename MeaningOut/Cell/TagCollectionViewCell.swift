4310756c49f291d734dbedc5f9e7694194a8d15b
홍정민 <dream7739@naver.com>
2024-06-15 22:36:50 +0900

    resultVC 정렬조건 collectionView 생성

---
+++ MeaningOut/Cell/TagCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TagCollectionViewCell.swift
+//  MeaningOut
+//
+//  Created by 홍정민 on 6/15/24.
+//
+
+import UIKit
+
+class TagCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MeaningOut/Custom/PaddingLabel.swift
@@ -0,0 +1,8 @@
+//
+//  PaddingLabel.swift
+//  MeaningOut
+//
+//  Created by 홍정민 on 6/15/24.
+//
+
+import Foundation
