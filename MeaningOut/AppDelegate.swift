7d582c5773eadfc4d2c0794fa8848d573f2ebc0c
홍정민 <dream7739@naver.com>
2024-06-18 18:08:00 +0900

    luanchScreen 2초간 유지되도록 변경

--- MeaningOut/AppDelegate.swift
+++ MeaningOut/AppDelegate.swift
@@ -11,10 +11,8 @@ import IQKeyboardManagerSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //Thread.sleep(forTimeInterval: 2)
+        Thread.sleep(forTimeInterval: 2)
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
         IQKeyboardManager.shared.resignOnTouchOutside = true
