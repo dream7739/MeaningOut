ae475c8a58d09458a198fe9869ac350e7df0d594
홍정민 <dream7739@naver.com>
2024-07-07 21:37:30 +0900

    tabBarVC appearence 적용

--- MeaningOut/ViewController/ShopTabBarController.swift
+++ MeaningOut/ViewController/ShopTabBarController.swift
@@ -11,9 +11,11 @@ final class ShopTabBarController : UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tabBar.unselectedItemTintColor = Design.ColorType.secondary
-        tabBar.tintColor = Design.ColorType.theme
+        configureTabBarController()
+        setUpTabBarAppearence()
+   }
+    
+    func configureTabBarController(){
         
         let searchVC = SearchViewController()
         let nav1 = UINavigationController(rootViewController: searchVC)
@@ -24,10 +26,32 @@ final class ShopTabBarController : UITabBarController {
         let settingVC = SettingViewController()
         let nav3 = UINavigationController(rootViewController: settingVC)
         
-        nav1.tabBarItem = UITabBarItem(title: "검색", image: Design.ImageType.search, tag: 0)
-        nav2.tabBarItem = UITabBarItem(title: "좋아요", image: Design.ImageType.like_unselected, tag: 1)
-        nav3.tabBarItem = UITabBarItem(title: "설정", image: Design.ImageType.profile, tag: 2)
+        nav1.tabBarItem = UITabBarItem(
+            title: "검색",
+            image: Design.ImageType.search,
+            tag: 0
+        )
+        nav2.tabBarItem = UITabBarItem(
+            title: "좋아요",
+            image: Design.ImageType.like_unselected,
+            tag: 1
+        )
+        nav3.tabBarItem = UITabBarItem(
+            title: "설정",
+            image: Design.ImageType.profile,
+            tag: 2
+        )
         
         setViewControllers([nav1, nav2, nav3], animated: true)
-   }
+    }
+    
+    
+    
+    func setUpTabBarAppearence(){
+        let apperance = UITabBarAppearance()
+        apperance.configureWithTransparentBackground()
+        tabBar.standardAppearance = apperance
+        tabBar.scrollEdgeAppearance = apperance
+        tabBar.tintColor = Design.ColorType.theme
+    }
 }
